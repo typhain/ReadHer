51e1edfc35a972ef638c964cb102303273446b9e
natnatparis <natnatparis1@gmail.com>
2019-09-09 16:45:36 +0200

    [update] schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_09_09_133251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "crushes", force: :cascade do |t|
+    t.string "book_title"
+    t.string "genre"
+    t.string "author_name"
+    t.string "author_country"
+    t.string "description"
+    t.string "quote"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
