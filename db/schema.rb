f1b67d6d6dab22c29a81ebb40cda84d42bdb8f5d
natnatparis <natnatparis1@gmail.com>
2019-09-09 20:24:22 +0200

    [update] seed and populate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_133251) do
+ActiveRecord::Schema.define(version: 2019_09_09_163144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 2019_09_09_133251) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "libraries", force: :cascade do |t|
+    t.bigint "users_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_libraries_on_users_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -39,4 +46,5 @@ ActiveRecord::Schema.define(version: 2019_09_09_133251) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "libraries", "users", column: "users_id"
 end
