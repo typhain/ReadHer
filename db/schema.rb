7d9ab3f2715917c1cd9519c7ac8a69d8edc684b8
typhain <typhaine.charrat@gmail.com>
2019-09-09 17:23:51 +0200

    [UPDATE] fichier production.rb configuré pour mailer en prod

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_09_09_133251) do
     t.string "quote"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
