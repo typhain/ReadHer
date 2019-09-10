76ae84ae757c6bb79e7d94f4a5d4a75f163e7456
natnatparis <natnatparis1@gmail.com>
2019-09-10 17:03:56 +0200

    [config] active storage and devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: exception 
+    #permit additionnal parameters in devise 
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :masquerade_user!
+
+    protected
+    
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:user])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:user])
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_one_attached :avatar
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :library
   has_many :crushes
+  has_one_attached :avatar
 end
