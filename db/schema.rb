893ae0343f41f36a60eada4120ee273012eccc69
natnatparis <natnatparis1@gmail.com>
2019-09-10 13:37:48 +0200

    [create] migration  database active storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_173639) do
+ActiveRecord::Schema.define(version: 2019_09_10_104452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "crush_libraries", force: :cascade do |t|
     t.bigint "crush_id"
@@ -31,17 +52,15 @@ ActiveRecord::Schema.define(version: 2019_09_09_173639) do
     t.string "author_country"
     t.string "description"
     t.string "quote"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_crushes_on_user_id"
   end
 
   create_table "libraries", force: :cascade do |t|
-    t.bigint "user_id"
+    t.bigint "users_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_libraries_on_user_id"
+    t.index ["users_id"], name: "index_libraries_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -59,8 +78,8 @@ ActiveRecord::Schema.define(version: 2019_09_09_173639) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "crush_libraries", "crushes"
   add_foreign_key "crush_libraries", "libraries"
-  add_foreign_key "crushes", "users"
-  add_foreign_key "libraries", "users"
+  add_foreign_key "libraries", "users", column: "users_id"
 end
