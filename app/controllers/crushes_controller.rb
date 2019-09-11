b20ace8ac4fa5ec4eed745f1900b2398b98bde13
typhain <typhaine.charrat@gmail.com>
2019-09-11 18:46:14 +0200

    [CREATE] form pour création de crush et new#crush

--- app/controllers/crushes_controller.rb
+++ app/controllers/crushes_controller.rb
@@ -8,7 +8,18 @@ class CrushesController < ApplicationController
     @crush = Crush.find(params[:id])
   end
 
+  def new
+    p "###############"
+  end
+
   def create
+    @user = current_user
+    @crush = Crush.new(:user => @user, :book_title=> params[:book_title], :genre => params[:genre], :author_name => params[:author_name], :author_country => params[:author_country], :description => params[:description], :quote => params[:quote])
+      if @crush.save
+        redirect_to root_path
+      else
+        render crushs_path
+      end
   end
 
   def update
