56c46293bf78bc89a0be399ab3c6a39a96abe4f7
natnatparis <natnatparis1@gmail.com>
2019-09-11 13:16:24 +0200

    [config] avatar dev and prod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     protected
     
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:user])
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:user])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :crushes
-  root to: 'crushes#index'
-  resources :users, only: [:show]
-  get "edit_user_registration" => "devise/registrations#edit"
+  root to: 'crushes#index' 
 end
