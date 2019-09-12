32a666a48b96aecb43bcb06366c90419460527ff
natnatparis <natnatparis1@gmail.com>
2019-09-12 21:38:21 +0200

    [update] action mailer

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,10 +1,19 @@
 class UserNotifierMailer < ApplicationMailer
-    default :from => 'any_from_address@example.com'
-  
-    # send a signup email to the user, pass in the user object that   contains the user's email address
-    def send_signup_email(user)
-      @user = user
-      mail( :to => @user.email,
-      :subject => 'Thanks for signing up for our amazing app' )
-    end
+  default :from => 'any_from_address@example.com'
+
+  # send a signup email to the user, pass in the user object that   contains the user's email address
+  def send_signup_email(user)
+    @user = user
+    mail( :to => @user.email,
+    :subject => 'Thanks for signing up for our amazing app' )
   end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:email)
+  end
+end
+
+
+  # send a signup email to the user, pass in the user object that   contains the user's email address
