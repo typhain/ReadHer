5698ddf30d94ff4fd6730456305869cf607f83d3
typhain <typhaine.charrat@gmail.com>
2019-09-13 17:57:08 +0200

    [UPDATE] reconfiguration sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,14 +63,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_options = { from: 'no-reply@monsite.fr' }
-  config.action_mailer.delivery_method = :letter_opener
 
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: '587',
-  domain: 'heroku.com',
+  domain: 'monsite.fr',
   user_name: ENV['SENDGRID_USERNAME'],
   password: ENV['SENDGRID_PASSWORD'],
   authentication: 'plain',
