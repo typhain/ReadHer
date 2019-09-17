6c11f938ef668d9875137e1da920fc9e5b5c5fbe
natnatparis <natnatparis1@gmail.com>
2019-09-17 14:33:25 +0200

    [create] username v1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar, :first_name,:last_name, :email, :city, :description])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar, :first_name,:last_name, :email, :city, :description, :username])
         devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :first_name, :last_name, :email, :city, :description])
     end
 end
