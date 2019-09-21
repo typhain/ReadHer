4ddb9f121122932fd8112665e842e4ea04fb3019
natnatparis <natnatparis1@gmail.com>
2019-09-21 02:14:07 +0200

    [create] static page v2

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   
   get 'users/show'
   get 'pages/about'
-  get 'pages/policy' 
+  get 'pages/policy'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :crushes
